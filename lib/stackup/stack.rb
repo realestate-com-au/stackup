25683488a26b237dea01eb635682becc15254200
Arvind Kunday <mail@kunday.com>
2015-09-14 14:53:47 +1000

    Add Stack Validation via CF validate_template. @matt-witherow

--- lib/stackup/stack.rb
+++ lib/stackup/stack.rb
@@ -1,14 +1,20 @@
 module Stackup
   class Stack
-    attr_accessor :stack
+    attr_accessor :stack, :cf, :template
 
-    def initialize(name)
-      cf = Aws::CloudFormation.new
+    def initialize(name, template)
+      @cf = Aws::CloudFormation.new
       @stack = cf.stacks[name]
+      @template = template
     end
 
     def deployed?
       stack.exists?
+    end
+
+    def valid?
+      response = cf.validate_template(template)
+      response[:code].nil?
     end
 
   end
--- spec/stackup/stack_spec.rb
+++ spec/stackup/stack_spec.rb
@@ -1,16 +1,29 @@
 require 'spec_helper'
 
 describe Stackup::Stack do
-  let(:stack) { Stackup::Stack.new('stack_name') }
+  let(:stack) { Stackup::Stack.new('stack_name', double(String)) }
   let(:cf_stack) { double(Aws::CloudFormation::Stack) }
-  context 'deployed' do
-    let(:cf) {double(Aws::CloudFormation)}
+  let(:cf) {double(Aws::CloudFormation)}
 
-    before do
-      allow(Aws::CloudFormation).to receive(:new).and_return(cf)
-      allow(cf).to receive(:stacks).and_return({'stack_name' => cf_stack})
+  before do
+    allow(Aws::CloudFormation).to receive(:new).and_return(cf)
+    allow(cf).to receive(:stacks).and_return({'stack_name' => cf_stack})
+  end
+
+  context 'validate' do
+    it 'should be valid if cf validate say so' do
+      allow(cf).to receive(:validate_template).and_return({})
+      expect(stack.valid?).to be true
     end
 
+    it 'should be invalid if cf validate say so' do
+      allow(cf).to receive(:validate_template).and_return({code: '404'})
+      expect(stack.valid?).to be false
+    end
+
+  end
+
+  context 'deployed' do
     it 'should be true if it is already deployed' do
       allow(cf_stack).to receive(:exists?).and_return(true)
       expect(stack.deployed?).to be true
