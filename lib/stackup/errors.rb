2f620ae74edc5d8b359124eb757eccde41eb8e45
Mike Williams <mdub@dogbiscuit.org>
2015-10-07 10:55:37 +1100

    Document exceptions.

--- lib/stackup/errors.rb
+++ lib/stackup/errors.rb
@@ -1,12 +1,15 @@
 module Stackup
 
+  # Base Stackup Exception class
   class ServiceError < StandardError
   end
 
+  # Raised when the specified stack does not exist
   class NoSuchStack < ServiceError
   end
 
-  class StackUpdateError < StandardError
+  # Raised to indicate a problem updating a stack
+  class StackUpdateError < ServiceError
   end
 
 end
