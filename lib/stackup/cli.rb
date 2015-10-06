0af2edf84cceb4bf101be782a7218d4564e77a05
Mike Williams <mdub@dogbiscuit.org>
2015-10-06 11:37:40 +1100

    Factor out `stack`.

--- lib/stackup/cli.rb
+++ lib/stackup/cli.rb
@@ -1,43 +1,51 @@
 module Stackup
+
   class CLI < Clamp::Command
 
-    subcommand ["stack"], "Manage a stack." do
-      parameter "STACK-NAME", "Name of stack", :attribute_name => :stack_name
+    subcommand "stack", "Manage a stack." do
+
+      parameter "NAME", "Name of stack", :attribute_name => :stack_name
+
+      private
+
+      def stack
+        Stackup::Stack.new(stack_name)
+      end
 
       subcommand "status", "Print stack status." do
 
         def execute
-          stack = Stackup::Stack.new(stack_name)
           puts stack.status
         end
 
       end
 
       subcommand "deploy", "Create/update the stack" do
+
         parameter "TEMPLATE", "CloudFormation template (.json)", :attribute_name => :template
         parameter "PARAMETERS", "CloudFormation parameters (.json)", :attribute_name => :parameters
 
         def execute
           params = JSON.parse(parameters)
-          stack = Stackup::Stack.new(stack_name)
-          stack.create(template, params)
+          stack.create(template, {})
         end
+
       end
 
       subcommand "delete", "Remove the stack." do
         def execute
-          stack = Stackup::Stack.new(stack_name)
           stack.delete
         end
       end
 
       subcommand "outputs", "Stack outputs." do
         def execute
-          stack = Stackup::Stack.new(stack_name)
           stack.outputs
         end
       end
+
     end
 
   end
+
 end
