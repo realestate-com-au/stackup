7cda02a1b787764fac6c992ac49bd8d30b2e240b
Mike Williams <mdub@dogbiscuit.org>
2015-10-09 09:00:53 +1100

    Recognise NoSuchStack on update.

--- lib/stackup/errors.rb
+++ lib/stackup/errors.rb
@@ -24,8 +24,8 @@ module Stackup
     case e.message
     when "No updates are to be performed."
       fail NoUpdateRequired, "no updates are required"
-    when /Stack with id (.+) does not exist$/
-      fail NoSuchStack, "no such stack: #{$1}"
+    when /Stack .* does not exist$/
+      fail NoSuchStack, "no such stack"
     when / cannot be called from current stack status$/
       fail InvalidStateError, e.message
     else
