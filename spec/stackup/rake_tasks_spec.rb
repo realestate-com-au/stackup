realestate-com-au/stackup

commit b76a4233b5a1f8b2c0b82cbb6ca4cdec994e5b87

Add support for CloudFormation capabilities

lib/stackup/rake_tasks.rb
@@ -1,6 +1,7 @@
 require "rake/tasklib"
 require "tempfile"
 require "yaml"
+require "Pathname"
 
 module Stackup
 
@@ -13,6 +14,7 @@ module Stackup
     attr_accessor :template
     attr_accessor :parameters
     attr_accessor :tags
+    attr_accessor :capabilities
 
     alias namespace= name=
 
@@ -37,14 +39,15 @@ module Stackup
     def define
       namespace(name) do
 
-        data_options = DataOptions.new
-        data_options["--template"] = template
-        data_options["--parameters"] = parameters if parameters
-        data_options["--tags"] = tags if tags
+        data_options = []
+        data_options += DataOption.for("--template", template).to_a
+        data_options += DataOption.for("--parameters", parameters).to_a if parameters
+        data_options += DataOption.for("--tags", tags).to_a if tags
+        data_options += DataOption.for("--capability", capabilities).to_a if capabilities
 
         desc "Update #{stack} stack"
-        task "up" => data_options.files do
-          stackup "up", *data_options.to_a
+        task "up" => data_options.grep(DataOptionFile) do
+          stackup "up", *data_options
         end
 
         desc "Cancel update of #{stack} stack"
@@ -53,8 +56,8 @@ module Stackup
         end
 
         desc "Show pending changes to #{stack} stack"
-        task "diff" => data_options.files do
-          stackup "diff", *data_options.to_a
+        task "diff" => data_options.grep(DataOptionFile) do
+          stackup "diff", *data_options
         end
 
         desc "Show #{stack} stack outputs and resources"
@@ -75,41 +78,68 @@ module Stackup
       end
     end
 
-    # Options to "stackup up".
-    #
-    class DataOptions
+    # A flag with optional argument that will be passed to stackup
+    class DataOption
 
-      def initialize
-        @options = {}
-      end
-
-      def []=(option, file_or_value)
-        @options[option] = file_or_value
-      end
-
-      def files
-        @options.values.grep(String)
+      def initialize(flag, argument)
+        @flag = flag
+        @argument = argument if argument
       end
 
       def to_a
-        [].tap do |result|
-          @options.each do |option, file_or_data|
-            result << option
-            result << maybe_tempfile(file_or_data, option[2..-1])
-          end
+        [@flag, @argument]
+      end
+
+      # Factory method for initialising DataOptions based on class
+      def self.for(flag, argument)
+        case argument
+        when Hash
+          DataOptionHash.new(flag, argument)
+        when Array
+          DataOptionArray.new(flag, argument)
+        when String && File.exist?(argument)
+          DataOptionFile.new(flag, argument)
+        else
+          DataOption.new(flag, argument)
         end
       end
 
-      def maybe_tempfile(file_or_data, type)
-        return file_or_data if file_or_data.is_a?(String)
+    end
 
-        tempfile = Tempfile.new([type, ".yml"])
-        tempfile.write(YAML.dump(file_or_data))
+    # An option with a Hash argument
+    # Hash content is stored in a temporary file upon conversion
+    class DataOptionHash < DataOption
+
+      def as_tempfile(filename, data)
+        tempfile = Tempfile.new(filename)
+        tempfile.write(YAML.dump(data))
         tempfile.close
         tempfile.path.to_s
       end
 
+      def to_a
+        [@flag, as_tempfile([@flag[2..-1], ".yml"], @argument)]
+      end
+
     end
+
+    # An option with an Array argument
+    # Flag is repeated for every Array member upon conversion
+    class DataOptionArray < DataOption
+
+      def to_a
+        [].tap do |result|
+          @argument.each do |argument|
+            result << @flag
+            result << argument
+          end
+        end
+      end
+
+    end
+
+    # An option with a File argument
+    class DataOptionFile < DataOption; end
 
   end
 

bin/stackup
@@ -201,6 +201,9 @@ Clamp do
            "when stack creation fails: DO_NOTHING, ROLLBACK, or DELETE",
            :default => "ROLLBACK"
 
+    option "--capability", "CAPABILITY", "cloudformation capability",
+           :multivalued => true, :default => ["CAPABILITY_NAMED_IAM"]
+
     def execute
       unless template_source || use_previous_template?
         signal_usage_error "Specify either --template or --use-previous-template"
@@ -225,6 +228,7 @@ Clamp do
       end
       options[:role_arn] = service_role_arn if service_role_arn
       options[:use_previous_template] = use_previous_template?
+      options[:capabilities] = capability_list
       report_change do
         stack.create_or_update(options)
       end
@@ -279,6 +283,9 @@ Clamp do
              :attribute_name => :tag_source,
              &Stackup::Source.method(:new)
 
+      option "--capability", "CAPABILITY", "cloudformation capability",
+             :multivalued => true, :default => ["CAPABILITY_NAMED_IAM"]
+
       def execute
         unless template_source || use_previous_template?
           signal_usage_error "Specify either --template or --use-previous-template"
@@ -296,6 +303,7 @@ Clamp do
         options[:tags] = tag_source.data if tag_source
         options[:use_previous_template] = use_previous_template?
         options[:force] = force?
+        options[:capabilities] = capability_list
         report_change do
           change_set.create(options)
         end

examples/Rakefile
@@ -5,7 +5,11 @@ Stackup::RakeTasks.new("demo") do |t|
   t.template = "template.json"
   t.parameters = "parameters-verbose.json"
   t.tags = {
-    "environment" => "dev",
-    "team" => "rea-oss"
+      "environment" => "dev",
+      "team" => "rea-oss"
   }
+  t.capabilities = [
+      "CAPABILITY_IAM",
+      "CAPABILITY_NAMED_IAM"
+  ]
 end

spec/stackup/stack_spec.rb
@@ -280,6 +280,22 @@ describe Stackup::Stack do
 
       end
 
+      context "with :capabilities as an Array" do
+
+        before do
+          options[:capabilities] = %w[CAPABILITY_IAM CAPABILITY_NAMED_IAM]
+        end
+
+        it "passes them through" do
+          expected_capabilities = %w[CAPABILITY_IAM CAPABILITY_NAMED_IAM]
+          create_or_update
+          expect(cf_client).to have_received(:create_stack) do |options|
+            expect(options[:capabilities]).to eq(expected_capabilities)
+          end
+        end
+
+      end
+
     end
 
     describe "#change_set#create" do
@@ -355,6 +371,22 @@ describe Stackup::Stack do
           create_change_set
           expect(cf_client).to have_received(:create_change_set) do |options|
             expect(options[:tags]).to eq(expected_tags)
+          end
+        end
+
+      end
+
+      context "with :capabilities as an Array" do
+
+        before do
+          options[:capabilities] = %w[CAPABILITY_IAM CAPABILITY_NAMED_IAM]
+        end
+
+        it "passes them through" do
+          expected_capabilities = %w[CAPABILITY_IAM CAPABILITY_NAMED_IAM]
+          create_change_set
+          expect(cf_client).to have_received(:create_change_set) do |options|
+            expect(options[:capabilities]).to eq(expected_capabilities)
           end
         end
 

commit 59fc037a7749fa3fe63ed559d40605df3c5e9fc3

Fixes Rake Tasks
Commit b76a423 broke the requires by importing "Pathname" instead of
"pathname"

lib/stackup/rake_tasks.rb
@@ -1,7 +1,7 @@
 require "rake/tasklib"
 require "tempfile"
 require "yaml"
-require "Pathname"
+require "pathname"
 
 module Stackup
 

spec/stackup/rake_tasks_spec.rb
@@ -1,0 +1,8 @@
+require 'spec_helper'
+require 'stackup/rake_tasks'
+
+RSpec.describe Stackup::RakeTasks do
+  it 'can be required' do
+    expect(described_class).to be_truthy
+  end
+end