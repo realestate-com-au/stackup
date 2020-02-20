e03a6561d85fc243ace2b5a70095bcb6a67e0676
Phil Jay <phil@jay.id.au>
2020-02-20 17:33:01 +1100

    Experiment with uploading the original template

--- lib/stackup/stack.rb
+++ lib/stackup/stack.rb
@@ -119,6 +119,10 @@ module Stackup
       if (template_data = options.delete(:template))
         options[:template_body] = MultiJson.dump(template_data)
       end
+      # override template_body with the original template to preserve formatting (& comments in YAML)
+      if (template_orig = options.delete(:template_orig))
+        options[:template_body] = template_orig
+      end
       if (parameters = options[:parameters])
         options[:parameters] = Parameters.new(parameters).to_a
       end
